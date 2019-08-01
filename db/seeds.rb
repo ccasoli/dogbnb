7a9a74ed83b7721f9f7ce078a703988db65df313
casoli <ccasoli@hotmail.fr>
2019-08-01 14:13:04 +0200

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do 
+    d =Dog.create(name: Faker::Creature::Dog.name, city_id: Faker::Number.within(range: 1..20))
+end
+
+60.times do
+    d =DogSitter.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city_id: Faker::Number.within(range: 1..20))
+end
+
+20.times do
+    d =City.create(name: Faker::Address.city)
+end
+
+30.times do
+    d =Stroll.create(dog_id: Faker::Number.within(range: 1..100), dog_sitter_id: Faker::Number.within(range: 1..60))
+end
